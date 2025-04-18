45904403f490bb452694b6dd20c320207789172b
bLeohr <benjamin.leohr@stu.daviess.kyschools.us>
2025-04-18 04:07:11 +0000

    coolio

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort
 		int[] myNums = {12, 15, 0, 44, 13, 1, 2};
 		int passes;
 		int comparisons;
-		int tempNum;
+		int tempNum=0;
 
 		//Loop once for each pass, where passes is one less than the number of items.
 			//Loop once for each comparison, where comparisons are one less than the number of unsorted.
@@ -17,7 +17,22 @@ public class BubbleSort
     
 		//End Loop for passes
 
+		for(int x: myNums)
+		System.out.print(x + " ");
 
 
+		for(int i=0; i<myNums.length-1; i++)
+			for(int j=0; j<myNums.length-1; j++)
+				if(myNums[j]>myNums[j+1]){
+				//Switch them if wrong
+					tempNum=myNums[j];
+					myNums[j]=myNums[j+1];
+					myNums[j+1]=tempNum;}
+
+
+					
+					System.out.print("\n");
+					for(int x: myNums)
+					System.out.print(x + " ");
 	}
 }
